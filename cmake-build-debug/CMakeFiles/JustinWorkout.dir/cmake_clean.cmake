ddf9377815a8479f46388f42f7a0699abea0ee61
Justin-De-Sio <desio.j@live.fr>
2021-08-23 11:20:24 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/JustinWorkout.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JustinWorkout.dir/main.cpp.obj"
+  "JustinWorkout.exe"
+  "JustinWorkout.exe.manifest"
+  "JustinWorkout.pdb"
+  "libJustinWorkout.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JustinWorkout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
